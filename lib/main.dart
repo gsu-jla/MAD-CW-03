3800a40f76544ca7b35ffb866f06adb0fc39be06
gsu-jla <jla2@student.gsu.edu>
2025-03-01 21:20:34 -0500

    makes it so user has to add to list via text input

--- lib/main.dart
+++ lib/main.dart
@@ -1,231 +1,84 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [ListTile] selection in a [ListView] or [GridView].
+void main() {
+  runApp(const MyApp());
+}
 
-void main() => runApp(const ListViewExampleApp());
-
-class ListViewExampleApp extends StatelessWidget {
-  const ListViewExampleApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: ListTileSelectExample());
+    return MaterialApp(
+      title: 'Flutter List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ListScreen(),
+    );
   }
 }
 
-class ListTileSelectExample extends StatefulWidget {
-  const ListTileSelectExample({super.key});
+class ListScreen extends StatefulWidget {
+  const ListScreen({super.key});
 
   @override
-  ListTileSelectExampleState createState() => ListTileSelectExampleState();
+  _ListScreenState createState() => _ListScreenState();
 }
 
-class ListTileSelectExampleState extends State<ListTileSelectExample> {
-  bool isSelectionMode = false;
-  final int listLength = 30;
-  late List<bool> _selected;
-  bool _selectAll = false;
-  bool _isGridMode = false;
+class _ListScreenState extends State<ListScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _items = [];
 
-  @override
-  void initState() {
-    super.initState();
-    initializeSelection();
-  }
-
-  void initializeSelection() {
-    _selected = List<bool>.generate(listLength, (_) => false);
-  }
-
-  @override
-  void dispose() {
-    _selected.clear();
-    super.dispose();
+  void _addItem() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _items.add(_controller.text);
+        _controller.clear();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ListTile selection'),
-        leading:
-            isSelectionMode
-                ? IconButton(
-                  icon: const Icon(Icons.close),
-                  onPressed: () {
-                    setState(() {
-                      isSelectionMode = false;
-                    });
-                    initializeSelection();
-                  },
-                )
-                : const SizedBox(),
-        actions: <Widget>[
-          if (_isGridMode)
-            IconButton(
-              icon: const Icon(Icons.grid_on),
-              onPressed: () {
-                setState(() {
-                  _isGridMode = false;
-                });
-              },
-            )
-          else
-            IconButton(
-              icon: const Icon(Icons.list),
-              onPressed: () {
-                setState(() {
-                  _isGridMode = true;
-                });
-              },
-            ),
-          if (isSelectionMode)
-            TextButton(
-              child:
-                  !_selectAll
-                      ? const Text('select all', style: TextStyle(color: Colors.white))
-                      : const Text('unselect all', style: TextStyle(color: Colors.white)),
-              onPressed: () {
-                _selectAll = !_selectAll;
-                setState(() {
-                  _selected = List<bool>.generate(listLength, (_) => _selectAll);
-                });
-              },
-            ),
-        ],
+        title: const Text('Flutter List View'),
       ),
-      body:
-          _isGridMode
-              ? GridBuilder(
-                isSelectionMode: isSelectionMode,
-                selectedList: _selected,
-                onSelectionChange: (bool x) {
-                  setState(() {
-                    isSelectionMode = x;
-                  });
-                },
-              )
-              : ListBuilder(
-                isSelectionMode: isSelectionMode,
-                selectedList: _selected,
-                onSelectionChange: (bool x) {
-                  setState(() {
-                    isSelectionMode = x;
-                  });
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      labelText: 'Enter item',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: _addItem,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_items[index]),
+                  );
                 },
               ),
-    );
-  }
-}
-
-class GridBuilder extends StatefulWidget {
-  const GridBuilder({
-    super.key,
-    required this.selectedList,
-    required this.isSelectionMode,
-    required this.onSelectionChange,
-  });
-
-  final bool isSelectionMode;
-  final ValueChanged<bool>? onSelectionChange;
-  final List<bool> selectedList;
-
-  @override
-  GridBuilderState createState() => GridBuilderState();
-}
-
-class GridBuilderState extends State<GridBuilder> {
-  void _toggle(int index) {
-    if (widget.isSelectionMode) {
-      setState(() {
-        widget.selectedList[index] = !widget.selectedList[index];
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: widget.selectedList.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (_, int index) {
-        return InkWell(
-          onTap: () => _toggle(index),
-          onLongPress: () {
-            if (!widget.isSelectionMode) {
-              setState(() {
-                widget.selectedList[index] = true;
-              });
-              widget.onSelectionChange!(true);
-            }
-          },
-          child: GridTile(
-            child: Container(
-              child:
-                  widget.isSelectionMode
-                      ? Checkbox(
-                        onChanged: (bool? x) => _toggle(index),
-                        value: widget.selectedList[index],
-                      )
-                      : const Icon(Icons.image),
             ),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class ListBuilder extends StatefulWidget {
-   ListBuilder({
-    super.key,
-    required this.selectedList,
-    required this.isSelectionMode,
-    required this.onSelectionChange,
-  });
-
-  final bool isSelectionMode;
-   List<bool> selectedList;
-  final ValueChanged<bool>? onSelectionChange;
-
-  @override
-  State<ListBuilder> createState() => _ListBuilderState();
-}
-
-class _ListBuilderState extends State<ListBuilder> {
-  void _toggle(int index) {
-    if (widget.isSelectionMode) {
-      setState(() {
-        widget.selectedList[index] = !widget.selectedList[index];
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.selectedList.length,
-      itemBuilder: (_, int index) {
-        return ListTile(
-          onTap: () => _toggle(index),
-          onLongPress: () {
-            if (!widget.isSelectionMode) {
-              setState(() {
-                widget.selectedList[index] = true;
-              });
-              widget.onSelectionChange!(true);
-            }
-          },
-          trailing:
-              widget.isSelectionMode
-                  ? Checkbox(
-                    value: widget.selectedList[index],
-                    onChanged: (bool? x) => _toggle(index),
-                  )
-                  : const SizedBox.shrink(),
-          title: Text('item test $index'),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 }
